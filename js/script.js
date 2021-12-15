b580445dd9d593421234fc756fa7426106b7af23
Ahamed Badsha <90891343+ahamedbadsha@users.noreply.github.com>
2021-12-15 19:41:09 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+//java code//
+function textChange(){
+
+  document.getElementById('c_prg').innerHTML = "my name is badsha";
+
+} 
+
+function imageChange1(){
+
+  document.getElementById('img1').src = "image/img1.jpg"
+
+}
+
+function imageChange2(){
+
+  document.getElementById('img1').src = "image/img2.jpg"
+
+}
+
+function welcome(){
+
+  window.alert("welcome");
+}
